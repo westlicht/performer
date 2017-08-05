5d9dfb035e2bfbd867abddd23ad0f3265c9184c9
Simon Kallweit <simon.kallweit@gmail.com>
2017-08-05 22:55:34 +0200

    dump file and line number in asserts

--- src/core/Debug.h
+++ src/core/Debug.h
@@ -10,7 +10,9 @@ void dbg_printf(char const *fmt, ...) __attribute__((__format__(__printf__, 1, 2
 # define DBG_LO() DebugPin::low()
 # define DBG_HI() DebugPin::high()
 void dbg_assert(bool cond, const char *fmt, ...) __attribute__((__format__(__printf__, 2, 3)));
-# define ASSERT(_cond_, _fmt_, ...) dbg_assert(_cond_, _fmt_ "\r\n", ##__VA_ARGS__)
+# define ASSERT_STR_HELPER(x) #x
+# define ASSERT_STR(x) ASSERT_STR_HELPER(x)
+# define ASSERT(_cond_, _fmt_, ...) dbg_assert(_cond_, "ASSERT in " __FILE__ ":" ASSERT_STR(__LINE__) "\r\n" _fmt_ "\r\n", ##__VA_ARGS__)
 #else // ENABLE_DEBUG
 # define DBG(_fmt_, ...)
 # define DBG_LO()
