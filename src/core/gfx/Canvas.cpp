c1250c73d0e9e9f6dff2b7cf176a86c1acc491b2
Simon Kallweit <simon.kallweit@gmail.com>
2017-08-07 22:50:36 +0200

    added textWidth() and fixed whitespace

--- src/core/gfx/Canvas.cpp
+++ src/core/gfx/Canvas.cpp
@@ -23,7 +23,7 @@ void Canvas::point(int x, int y) {
     switch (_blendMode) {
     case BlendMode::Set: point<blit::set>(x, y); break;
     case BlendMode::Add: point<blit::add>(x, y); break;
-    case BlendMode::Sub: point<blit::sub>(x, y); break;    
+    case BlendMode::Sub: point<blit::sub>(x, y); break;
     }
 }
 
@@ -31,7 +31,7 @@ void Canvas::hline(int x, int y, int w) {
     switch (_blendMode) {
     case BlendMode::Set: hline<blit::set>(x, y, w); break;
     case BlendMode::Add: hline<blit::add>(x, y, w); break;
-    case BlendMode::Sub: hline<blit::sub>(x, y, w); break;    
+    case BlendMode::Sub: hline<blit::sub>(x, y, w); break;
     }
 }
 
@@ -39,7 +39,7 @@ void Canvas::vline(int x, int y, int h) {
     switch (_blendMode) {
     case BlendMode::Set: vline<blit::set>(x, y, h); break;
     case BlendMode::Add: vline<blit::add>(x, y, h); break;
-    case BlendMode::Sub: vline<blit::sub>(x, y, h); break;    
+    case BlendMode::Sub: vline<blit::sub>(x, y, h); break;
     }
 }
 
@@ -47,7 +47,7 @@ void Canvas::line(float x0, float y0, float x1, float y1) {
     switch (_blendMode) {
     case BlendMode::Set: line<blit::set>(x0, y0, x1, y1); break;
     case BlendMode::Add: line<blit::add>(x0, y0, x1, y1); break;
-    case BlendMode::Sub: line<blit::sub>(x0, y0, x1, y1); break;    
+    case BlendMode::Sub: line<blit::sub>(x0, y0, x1, y1); break;
     }
 }
 
@@ -55,7 +55,7 @@ void Canvas::drawRect(int x, int y, int w, int h) {
     switch (_blendMode) {
     case BlendMode::Set: drawRect<blit::set>(x, y, w, h); break;
     case BlendMode::Add: drawRect<blit::add>(x, y, w, h); break;
-    case BlendMode::Sub: drawRect<blit::sub>(x, y, w, h); break;    
+    case BlendMode::Sub: drawRect<blit::sub>(x, y, w, h); break;
     }
 }
 
@@ -63,7 +63,7 @@ void Canvas::fillRect(int x, int y, int w, int h) {
     switch (_blendMode) {
     case BlendMode::Set: fillRect<blit::set>(x, y, w, h); break;
     case BlendMode::Add: fillRect<blit::add>(x, y, w, h); break;
-    case BlendMode::Sub: fillRect<blit::sub>(x, y, w, h); break;    
+    case BlendMode::Sub: fillRect<blit::sub>(x, y, w, h); break;
     }
 }
 
@@ -71,7 +71,7 @@ void Canvas::drawBitmap1bit(int x, int y, int w, int h, const uint8_t *bitmap) {
     switch (_blendMode) {
     case BlendMode::Set: drawBitmap<blit::set, 1>(x, y, w, h, bitmap); break;
     case BlendMode::Add: drawBitmap<blit::add, 1>(x, y, w, h, bitmap); break;
-    case BlendMode::Sub: drawBitmap<blit::sub, 1>(x, y, w, h, bitmap); break;    
+    case BlendMode::Sub: drawBitmap<blit::sub, 1>(x, y, w, h, bitmap); break;
     }
 }
 
@@ -79,7 +79,7 @@ void Canvas::drawBitmap4bit(int x, int y, int w, int h, const uint8_t *bitmap) {
     switch (_blendMode) {
     case BlendMode::Set: drawBitmap<blit::set, 4>(x, y, w, h, bitmap); break;
     case BlendMode::Add: drawBitmap<blit::add, 4>(x, y, w, h, bitmap); break;
-    case BlendMode::Sub: drawBitmap<blit::sub, 4>(x, y, w, h, bitmap); break;    
+    case BlendMode::Sub: drawBitmap<blit::sub, 4>(x, y, w, h, bitmap); break;
     }
 }
 
@@ -110,5 +110,17 @@ void Canvas::drawText(int x, int y, const char *str) {
 }
 
 int Canvas::textWidth(const char *str) {
-    return 0;
+    const auto &font = bitmapFont(_font);
+    int width = 0;
+
+    while (*str != '\0') {
+        auto c = *str++;
+        if (c < font.first || c > font.last) {
+            continue;
+        }
+        const auto &g = font.glyphs[c - font.first];
+        width += g.width;
+    }
+
+    return width;
 }
