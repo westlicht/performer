eb0a0bc364dc2f16fcbca739cf830a88511ffc45
Simon Kallweit <simon.kallweit@gmail.com>
2017-08-18 09:46:24 +0200

    MIDI USART initialization

--- src/platform/stm32/drivers/MIDI.cpp
+++ src/platform/stm32/drivers/MIDI.cpp
@@ -6,11 +6,33 @@
 #include <libopencm3/cm3/nvic.h>
 
 #define MIDI_USART USART2
+#define MIDI_RCC RCC_USART2
+#define MIDI_NVIC_IRQ NVIC_USART2_IRQ
 
 static MIDI *g_midi = nullptr;
 
 void MIDI::init() {
     g_midi = this;
+
+#if 0
+    // setup GPIO pins
+    rcc_periph_clock_enable(RCC_GPIOD);
+    gpio_mode_setup(GPIOD, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO8 | GPIO9);
+    gpio_set_af(GPIOD, GPIO_AF7, GPIO8 | GPIO9);
+
+    // setup usart
+    rcc_periph_clock_enable(MIDI_RCC);
+    usart_set_baudrate(MIDI_USART, 31250);
+    usart_set_databits(MIDI_USART, 8);
+    usart_set_stopbits(MIDI_USART, USART_STOPBITS_1);
+    usart_set_mode(MIDI_USART, USART_MODE_TX_RX);
+    usart_set_parity(MIDI_USART, USART_PARITY_NONE);
+    usart_set_flow_control(MIDI_USART, USART_FLOWCONTROL_NONE);
+    usart_enable(MIDI_USART);
+
+    // nvic_set_priority(CONSOLE_NVIC_IRQ, configMAX_SYSCALL_INTERRUPT_PRIORITY);
+    nvic_enable_irq(MIDI_NVIC_IRQ);
+#endif
 }
 
 void MIDI::send(const MIDIMessage &message) {
