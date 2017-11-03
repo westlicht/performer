a0a204917ec9804bc719c83edfd03254641fe163
Simon Kallweit <simon.kallweit@gmail.com>
2017-11-03 22:34:50 +0100

    LCD using DMA controller
    this is work in progress and still has some issues to be fixed, so
    disabled for now

--- src/platform/stm32/drivers/LCD.cpp
+++ src/platform/stm32/drivers/LCD.cpp
@@ -1,8 +1,10 @@
 #include "LCD.h"
 
+#include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/spi.h>
+#include <libopencm3/stm32/dma.h>
 
 #include <cmath>
 #include <algorithm>
@@ -14,11 +16,18 @@
 #define LCD_GPIO (LCD_CS | LCD_RES | LCD_DC)
 
 #define LCD_SPI SPI3
+#define LCD_SPI_DR SPI3_DR
+
+#define LCD_DMA DMA1
+#define LCD_DMA_CHANNEL DMA_SxCR_CHSEL_0
+#define LCD_DMA_STREAM DMA_STREAM5
 
 #define SPI_PORT GPIOC
 #define SPI_SCK GPIO10
 #define SPI_MOSI GPIO12
 #define SPI_GPIO (SPI_SCK | SPI_MOSI)
+
+// #define USE_DMA
 
 static void wait(int n) {
     for (volatile int i = 0; i < n; ++i) {
@@ -27,7 +36,6 @@ static void wait(int n) {
 }
 
 void LCD::init() {
-    // rcc_periph_clock_enable(RCC_GPIOC);
     rcc_periph_clock_enable(RCC_GPIOC);
 
     // init spi pins
@@ -41,12 +49,21 @@ void LCD::init() {
                     SPI_CR1_CPHA_CLK_TRANSITION_1,
                     SPI_CR1_DFF_8BIT,
                     SPI_CR1_MSBFIRST);
+    spi_set_unidirectional_mode(LCD_SPI);
+    spi_disable_crc(LCD_SPI);
+    spi_enable_ss_output(LCD_SPI);
     spi_enable(LCD_SPI);
+
+#ifdef USE_DMA
+    // init dma
+    rcc_periph_clock_enable(RCC_DMA1);
+    nvic_enable_irq(NVIC_DMA1_STREAM5_IRQ);
+    dma_stream_reset(LCD_DMA, LCD_DMA_STREAM);
+#endif
 
     // init control pins
     gpio_mode_setup(LCD_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LCD_GPIO);
     gpio_clear(LCD_PORT, LCD_GPIO);
-
 
     // setup
     gpio_clear(LCD_PORT, LCD_CS);
@@ -120,6 +137,16 @@ void LCD::init() {
 }
 
 void LCD::draw(uint8_t *frameBuffer) {
+#ifdef USE_DMA
+    // wait for transfer to complete
+    // while (DMA_SCR(LCD_DMA, LCD_DMA_STREAM) & DMA_SxCR_EN);
+    // dma_disable_stream(LCD_DMA, LCD_DMA_STREAM);
+    // dma_disable_stream(LCD_DMA, LCD_DMA_STREAM);
+    dma_stream_reset(LCD_DMA, LCD_DMA_STREAM);
+    // dma_clear_interrupt_flags(LCD_DMA, LCD_DMA_STREAM, DMA_TCIF);
+    // spi_disable_tx_dma(LCD_SPI);
+#endif
+
     uint8_t *src = frameBuffer;
     uint8_t *dst = reinterpret_cast<uint8_t *>(_frameBuffer);
     for (int i = 0; i < Cols * Rows / 2; ++i) {
@@ -129,6 +156,39 @@ void LCD::draw(uint8_t *frameBuffer) {
         // *dst++ = (b >> 4) | ((a >> 4) << 4);
     }
 
+
+#ifdef USE_DMA
+    setColAddr(0x1c,0x5b);
+    setRowAddr(0x00,0x3f);
+    setWrite();
+    gpio_set(LCD_PORT, LCD_DC);
+
+    dma_set_peripheral_address(LCD_DMA, LCD_DMA_STREAM, uint32_t(&LCD_SPI_DR));
+    dma_set_memory_address(LCD_DMA, LCD_DMA_STREAM, uint32_t(_frameBuffer));
+    dma_set_number_of_data(LCD_DMA, LCD_DMA_STREAM, sizeof(_frameBuffer));
+    dma_channel_select(LCD_DMA, LCD_DMA_STREAM, LCD_DMA_CHANNEL);
+    dma_set_priority(LCD_DMA, LCD_DMA_STREAM, DMA_SxCR_PL_HIGH);
+
+    dma_set_transfer_mode(LCD_DMA, LCD_DMA_STREAM, DMA_SxCR_DIR_MEM_TO_PERIPHERAL);
+    dma_enable_direct_mode(LCD_DMA, LCD_DMA_STREAM);
+    dma_set_memory_size(LCD_DMA, LCD_DMA_STREAM, DMA_SxCR_MSIZE_8BIT);
+    dma_set_peripheral_size(LCD_DMA, LCD_DMA_STREAM, DMA_SxCR_PSIZE_8BIT);
+
+    dma_enable_memory_increment_mode(LCD_DMA, LCD_DMA_STREAM);
+    dma_disable_peripheral_increment_mode(LCD_DMA, LCD_DMA_STREAM);
+    dma_set_memory_burst(LCD_DMA, LCD_DMA_STREAM, DMA_SxCR_PBURST_SINGLE);
+
+    dma_enable_stream(LCD_DMA, LCD_DMA_STREAM);
+    spi_enable_tx_dma(LCD_SPI);
+
+    while (DMA_SCR(LCD_DMA, LCD_DMA_STREAM) & DMA_SxCR_EN);
+
+    dma_disable_stream(LCD_DMA, LCD_DMA_STREAM);
+    spi_disable_tx_dma(LCD_SPI);
+
+    gpio_clear(LCD_PORT, LCD_DC);
+
+#else
     setColAddr(0x1c,0x5b);
     setRowAddr(0x00,0x3f);
     setWrite();
@@ -139,6 +199,7 @@ void LCD::draw(uint8_t *frameBuffer) {
             sendData(*src++);
         }
     }
+#endif
 }
 
 
