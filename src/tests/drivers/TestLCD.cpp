be6c555e1a6f1f8c8478f9a17391d5aadd46879b
Simon Kallweit <simon.kallweit@gmail.com>
2017-11-03 22:58:34 +0100

    added LCD test

---
+++ src/tests/drivers/TestLCD.cpp
@@ -0,0 +1,42 @@
+#include "test/TestRunner.h"
+
+#include "drivers/LCD.h"
+
+#include "core/gfx/FrameBuffer.h"
+#include "core/gfx/Canvas.h"
+
+#include "os/os.h"
+
+class TestLCD : public Test {
+public:
+    TestLCD() :
+        frameBuffer(256, 64, frameBufferData),
+        canvas(frameBuffer)
+    {
+    }
+
+    void init() {
+        lcd.init();
+    }
+
+    void update() {
+        int frame = os::ticks() / os::time::ms(33); // around 30 fps
+
+        canvas.setColor(0);
+        canvas.fill();
+
+        canvas.setColor(0xf);
+        canvas.vline(frame % 256, 0, 64);
+        canvas.hline(0, frame % 64, 256);
+
+        lcd.draw(frameBuffer.data());
+    }
+
+private:
+    uint8_t frameBufferData[256*64];
+    FrameBuffer8bit frameBuffer;
+    Canvas canvas;
+    LCD lcd;
+};
+
+TEST(TestLCD)
