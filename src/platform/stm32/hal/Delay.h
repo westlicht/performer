3265cb92d2fa45efe73dad7eb0bc254a4b7ad815
Simon Kallweit <simon.kallweit@gmail.com>
2017-11-03 22:35:21 +0100

    delay functions are static now

--- src/platform/stm32/hal/Delay.h
+++ src/platform/stm32/hal/Delay.h
@@ -8,26 +8,26 @@ namespace hal {
 namespace Delay {
 
 template<uint64_t cycles>
-void nop() {
+static void nop() {
     asm volatile ("nop");
     nop<cycles - 1>();
 }
 template<>
 void nop<0>() {}
 
-constexpr uint64_t ceil(double num) {
+static constexpr uint64_t ceil(double num) {
     return (static_cast<double>(static_cast<uint64_t>(num)) == num)
         ? static_cast<uint64_t>(num)
         : static_cast<uint64_t>(num) + ((num > 0) ? 1 : 0);
 }
 
-constexpr uint64_t ns_to_cycles(uint64_t ns) {
+static constexpr uint64_t ns_to_cycles(uint64_t ns) {
     return ceil(double(ns) / (double(1000000000) / double(CONFIG_CPU_FREQUENCY)));
 }
 
 // nanosecond delay based on nops
 template<uint64_t ns>
-void delay_ns() {
+static void delay_ns() {
     nop<ns_to_cycles(ns)>();
 }
 
