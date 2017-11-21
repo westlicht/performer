9aae665e3711521cd9a4dacaba8cc4bcbf63b6b8
Simon Kallweit <simon.kallweit@gmail.com>
2017-11-21 22:46:36 +0100

    working DIO driver (no interrupt driven inputs yet)

--- src/platform/stm32/drivers/DIO.cpp
+++ src/platform/stm32/drivers/DIO.cpp
@@ -1,4 +1,12 @@
 #include "DIO.h"
 
+#include <libopencm3/stm32/rcc.h>
+
 void DIO::init() {
+    rcc_periph_clock_enable(RCC_GPIOB);
+
+    clockInput.init();
+    resetInput.init();
+    clockOutput.init();
+    resetOutput.init();
 }
--- src/platform/stm32/drivers/DIO.h
+++ src/platform/stm32/drivers/DIO.h
@@ -1,28 +1,49 @@
 #pragma once
 
+#include <libopencm3/stm32/gpio.h>
+
 #include <functional>
 
 class DIO {
 public:
+    template<uint32_t Port, uint32_t Pin>
     struct Input {
         typedef std::function<void(bool)> Handler;
 
-        bool get() const { return false; }
+        void init() {
+            gpio_mode_setup(Port, GPIO_MODE_INPUT, GPIO_PUPD_NONE, Pin);
+        }
+
+        bool get() const {
+            return !gpio_get(Port, Pin);
+        }
+
         void setHandler(Handler handler) { _handler = handler; }
 
     private:
         Handler _handler;
     };
 
+    template<uint32_t Port, uint32_t Pin>
     struct Output {
-        void set(bool value) {}
+        void init() {
+            gpio_mode_setup(Port, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, Pin);
+        }
+
+        void set(bool value) {
+            if (value) {
+                gpio_clear(Port, Pin);
+            } else {
+                gpio_set(Port, Pin);
+            }
+        }
     };
 
     void init();
 
-    Input clockInput;
-    Input resetInput;
+    Input<GPIOB, GPIO11> clockInput;
+    Input<GPIOB, GPIO10> resetInput;
 
-    Output clockOutput;
-    Output resetOutput;
+    Output<GPIOB, GPIO2> clockOutput;
+    Output<GPIOB, GPIO1> resetOutput;
 };
