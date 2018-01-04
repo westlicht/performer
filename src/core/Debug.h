033814667e52d7f56ec7fe87af6d10ca03549a5b
Simon Kallweit <simon.kallweit@gmail.com>
2018-01-04 11:17:47 +0100

    removed writing carriage return when logging
    this is now done implicitely by the console driver

--- src/core/Debug.h
+++ src/core/Debug.h
@@ -4,17 +4,25 @@
 
 #include "drivers/DebugPin.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #if CONFIG_ENABLE_DEBUG
 void dbg_printf(char const *fmt, ...) __attribute__((__format__(__printf__, 1, 2)));
-# define DBG(_fmt_, ...) dbg_printf(_fmt_ "\r\n", ##__VA_ARGS__)
+# define DBG(_fmt_, ...) dbg_printf(_fmt_ "\n", ##__VA_ARGS__)
 # define DBG_LO() DebugPin::low()
 # define DBG_HI() DebugPin::high()
 void dbg_assert(bool cond, const char *fmt, ...) __attribute__((__format__(__printf__, 2, 3)));
 # define ASSERT_STR_HELPER(x) #x
 # define ASSERT_STR(x) ASSERT_STR_HELPER(x)
-# define ASSERT(_cond_, _fmt_, ...) dbg_assert(_cond_, "ASSERT in " __FILE__ ":" ASSERT_STR(__LINE__) "\r\n" _fmt_ "\r\n", ##__VA_ARGS__)
+# define ASSERT(_cond_, _fmt_, ...) dbg_assert(_cond_, "ASSERT in " __FILE__ ":" ASSERT_STR(__LINE__) "\n" _fmt_ "\n", ##__VA_ARGS__)
 #else // CONFIG_ENABLE_DEBUG
 # define DBG(_fmt_, ...)
 # define DBG_LO()
 # define DBG_HI()
 #endif // CONFIG_ENABLE_DEBUG
+
+#ifdef __cplusplus
+}
+#endif
