7ad5de63b6b168aee9bfa2f42938f32f8164ce61
Simon Kallweit <simon.kallweit@gmail.com>
2018-01-16 22:46:31 +0100

    fixed TestDIO for simulator

--- src/tests/integration/drivers/TestDIO.cpp
+++ src/tests/integration/drivers/TestDIO.cpp
@@ -20,23 +20,21 @@ public:
     }
 
     void update() override {
-        bool clockOutput = false;
-        bool resetOutput = true;
-        while (true) {
-            DBG("clock input: %s (called: %d)", dio.clockInput.get() ? "high" : "low", clockInputCount);
-            DBG("reset input: %s (called: %d)", dio.resetInput.get() ? "high" : "low", resetInputCount);
-            DBG("clock output: %s", clockOutput ? "high" : "low");
-            DBG("reset output: %s", clockOutput ? "high" : "low");
-            dio.clockOutput.set(clockOutput);
-            dio.resetOutput.set(resetOutput);
-            clockOutput = !clockOutput;
-            resetOutput = !resetOutput;
-            os::delay(os::time::ms(500));
-        }
+        DBG("clock input: %s (called: %d)", dio.clockInput.get() ? "high" : "low", clockInputCount);
+        DBG("reset input: %s (called: %d)", dio.resetInput.get() ? "high" : "low", resetInputCount);
+        DBG("clock output: %s", clockOutput ? "high" : "low");
+        DBG("reset output: %s", clockOutput ? "high" : "low");
+        dio.clockOutput.set(clockOutput);
+        dio.resetOutput.set(resetOutput);
+        clockOutput = !clockOutput;
+        resetOutput = !resetOutput;
+        os::delay(os::time::ms(500));
     }
 
 private:
     DIO dio;
+    bool clockOutput = false;
+    bool resetOutput = true;
     int clockInputCount = 0;
     int resetInputCount = 0;
 };
