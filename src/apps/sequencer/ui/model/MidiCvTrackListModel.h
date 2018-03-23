c0b0c36151ff7765cc72738e38661d70af07d2bc
Simon Kallweit <simon.kallweit@gmail.com>
2018-03-24 00:19:42 +0100

    added missing files

---
+++ src/apps/sequencer/ui/model/MidiCvTrackListModel.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "Config.h"
+
+#include "ListModel.h"
+
+#include "model/MidiCvTrack.h"
+
+class MidiCvTrackListModel : public ListModel {
+public:
+    void setTrack(MidiCvTrack &track) {
+        _track = &track;
+    }
+
+    virtual int rows() const override {
+        return Last;
+    }
+
+    virtual int columns() const override {
+        return 2;
+    }
+
+    virtual void cell(int row, int column, StringBuilder &str) const override {
+        if (column == 0) {
+            formatName(Item(row), str);
+        } else if (column == 1) {
+            formatValue(Item(row), str);
+        }
+    }
+
+    virtual void edit(int row, int column, int value, bool shift) override {
+        if (column == 1) {
+            editValue(Item(row), value, shift);
+        }
+    }
+
+private:
+    enum Item {
+        Port,
+        Channel,
+        Config,
+        Last
+    };
+
+    static const char *itemName(Item item) {
+        switch (item) {
+        case Port:      return "Port";
+        case Channel:   return "Channel";
+        case Config:    return "Config";
+        case Last:      break;
+        }
+        return nullptr;
+    }
+
+    void formatName(Item item, StringBuilder &str) const {
+        str(itemName(item));
+    }
+
+    void formatValue(Item item, StringBuilder &str) const {
+        switch (item) {
+        case Port:
+            _track->printPort(str);
+            break;
+        case Channel:
+            _track->printChannel(str);
+            break;
+        case Config:
+            _track->printConfig(str);
+            break;
+        case Last:
+            break;
+        }
+    }
+
+    void editValue(Item item, int value, bool shift) {
+        switch (item) {
+        case Port:
+            _track->editPort(value, shift);
+            break;
+        case Channel:
+            _track->editChannel(value, shift);
+            break;
+        case Config:
+            _track->editConfig(value, shift);
+            break;
+        case Last:
+            break;
+        }
+    }
+
+    MidiCvTrack *_track;
+};
---
+++ src/apps/sequencer/ui/model/NoteTrackListModel.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include "Config.h"
+
+#include "ListModel.h"
+
+#include "model/NoteTrack.h"
+
+class NoteTrackListModel : public ListModel {
+public:
+    void setTrack(NoteTrack &track) {
+        _track = &track;
+    }
+
+    virtual int rows() const override {
+        return Last;
+    }
+
+    virtual int columns() const override {
+        return 2;
+    }
+
+    virtual void cell(int row, int column, StringBuilder &str) const override {
+        if (column == 0) {
+            formatName(Item(row), str);
+        } else if (column == 1) {
+            formatValue(Item(row), str);
+        }
+    }
+
+    virtual void edit(int row, int column, int value, bool shift) override {
+        if (column == 1) {
+            editValue(Item(row), value, shift);
+        }
+    }
+
+private:
+    enum Item {
+        PlayMode,
+        FillMode,
+        Last
+    };
+
+    static const char *itemName(Item item) {
+        switch (item) {
+        case PlayMode:  return "Play Mode";
+        case FillMode:  return "Fill Mode";
+        case Last:      break;
+        }
+        return nullptr;
+    }
+
+    void formatName(Item item, StringBuilder &str) const {
+        str(itemName(item));
+    }
+
+    void formatValue(Item item, StringBuilder &str) const {
+        switch (item) {
+        case PlayMode:
+            _track->printPlayMode(str);
+            break;
+        case FillMode:
+            _track->printFillMode(str);
+            break;
+        case Last:
+            break;
+        }
+    }
+
+    void editValue(Item item, int value, bool shift) {
+        switch (item) {
+        case PlayMode:
+            _track->editPlayMode(value, shift);
+            break;
+        case FillMode:
+            _track->editFillMode(value, shift);
+            break;
+        case Last:
+            break;
+        }
+    }
+
+    NoteTrack *_track;
+};
