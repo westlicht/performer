982759cc4072b9a3b25339d9040a7622eb279d71
Simon Kallweit <simon.kallweit@gmail.com>
2018-04-01 11:36:10 +0200

    missing math stuff

--- src/core/math/Math.h
+++ src/core/math/Math.h
@@ -11,3 +11,13 @@ static T clamp(T value, T min, T max) {
 
 uint32_t nextPowerOfTwo(uint32_t value, bool strict = true);
 uint32_t prevPowerOfTwo(uint32_t value, bool strict = true);
+
+template<typename T>
+bool multipleBitsSet(T x) {
+    return x & (x - 1);
+}
+
+template<typename T>
+bool exactlyOneBitSet(T x) {
+    return x && !multipleBitsSet(x);
+}
