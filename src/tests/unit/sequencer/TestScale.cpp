e1f61cf9527d9c5d9472fe4ecd69afcc222c1d3f
Simon Kallweit <simon.kallweit@gmail.com>
2018-04-11 21:54:14 +0200

    added noteFromVolts case to TestScale

--- src/tests/unit/sequencer/TestScale.cpp
+++ src/tests/unit/sequencer/TestScale.cpp
@@ -10,7 +10,7 @@
 
 UNIT_TEST("Scale") {
 
-    CASE("scales") {
+    CASE("noteName/noteVolts") {
         for (int i = 0; i < Scale::Count; ++i) {
             const auto &scale = Scale::get(i);
             int notesPerOctave = scale.notesPerOctave();
@@ -32,9 +32,36 @@ UNIT_TEST("Scale") {
                 DBG("%-8d %-8.3f %-8s %-8s %-8s", note, volts, (const char *)(short1Name), (const char *)(short2Name), (const char *)(longName));
                 // DBG("note = %d, volts = %f", note, volts);
             }
+        }
+    }
+
+    CASE("noteFromVolts") {
+        for (int i = 0; i < Scale::Count; ++i) {
+            const auto &scale = Scale::get(i);
+            int notesPerOctave = scale.notesPerOctave();
+
+            DBG("----------------------------------------");
+            DBG("%s", scale.name());
+            DBG("octave has %d notes", notesPerOctave);
+            DBG("----------------------------------------");
+            DBG("%-8s %-8s %-8s %-8s %-8s %-8s", "voltsin", "note", "volts", "short1", "short2", "long");
+
+            for (int index = -12; index <= 12; ++index) {
+                float voltsin = index * (1.f / 12.f);
+                int note = scale.noteFromVolts(voltsin);
+                FixedStringBuilder<8> short1Name;
+                FixedStringBuilder<8> short2Name;
+                FixedStringBuilder<16> longName;
+                scale.noteName(short1Name, note, Scale::Short1);
+                scale.noteName(short2Name, note, Scale::Short2);
+                scale.noteName(longName, note, Scale::Long);
+                float volts = scale.noteVolts(note);
+                DBG("%-8.3f %-8d %-8.3f %-8s %-8s %-8s", voltsin, note, volts, (const char *)(short1Name), (const char *)(short2Name), (const char *)(longName));
+                // DBG("note = %d, volts = %f", note, volts);
+            }
 
         }
-
     }
+
 
 }
