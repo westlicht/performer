99521dd6b0bd9c9d159dfa9a875e242e9067091c
Simon Kallweit <simon.kallweit@gmail.com>
2018-04-16 15:30:52 +0200

    cleanup system driver

--- src/platform/stm32/drivers/System.cpp
+++ src/platform/stm32/drivers/System.cpp
@@ -6,14 +6,8 @@
 
 volatile uint32_t System::_ticks;
 
-
-// /* Called when systick fires */
-// void sys_tick_handler(void) {
-//     System::tick();
-// }
-
 void System::init() {
-    /* Base board frequency, set to 168Mhz */
+    // Base board frequency is 168MHz
     rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
 
     // clock frequencies:
@@ -21,14 +15,10 @@ void System::init() {
     // apb1_frequency = 42000000
     // apb2_frequency = 84000000
 
-
-    /* 1ms clock rate */
+    // 1ms clock rate
     systick_set_reload(168000000 / 1000);
     systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
     systick_counter_enable();
-
-    /* this done last */
-    systick_interrupt_enable();
 }
 
 void System::tick() {
