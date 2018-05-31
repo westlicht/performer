f3044b12265171ed116af11d6c8af8de792bd2ff
Simon Kallweit <simon.kallweit@gmail.com>
2018-05-31 22:33:20 +0200

    added fnv hashing class

---
+++ src/core/hash/FnvHash.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdint>
+
+class FnvHash {
+public:
+    uint32_t result() const { return _hash; }
+
+    void operator()(uint8_t data) {
+        _hash ^= data;
+        _hash *= Prime;
+    }
+
+    void operator()(const void *data, size_t len) {
+        const uint8_t *src = reinterpret_cast<const uint8_t *>(data);
+        while (len-- > 0) {
+            (*this)(*src++);
+        }
+    }
+
+private:
+    static constexpr uint32_t Hash = 0x811c9dc5;
+    static constexpr uint32_t Prime = 0x1000193;
+
+    uint32_t _hash = Hash;
+};
