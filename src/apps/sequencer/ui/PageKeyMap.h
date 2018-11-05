dc146dd1f45a27efaeebec658d643d9297c5929b
Simon Kallweit <simon.kallweit@gmail.com>
2018-11-05 21:38:17 +0100

    changed page map

--- src/apps/sequencer/ui/PageKeyMap.h
+++ src/apps/sequencer/ui/PageKeyMap.h
@@ -2,6 +2,9 @@
 
 #include "Key.h"
 
+//  Project     Layout      Routing     UserScale   ...     System
+//  SequenceEdt Sequence    Track       Song        ...     Monitor
+
 namespace PageKeyMap {
 
     enum Page {
@@ -11,15 +14,28 @@ namespace PageKeyMap {
 
         Project         = Key::Track0,
         Layout          = Key::Track1,
-        Track           = Key::Track2,
-        Sequence        = Key::Track3,
-        SequenceEdit    = Key::Track4,
-        Song            = Key::Track6,
-        Routing         = Key::Track7,
+        Routing         = Key::Track2,
+        UserScale       = Key::Track3,
 
-        UserScale       = Key::Step0,
-        Monitor         = Key::Step6,
-        System          = Key::Step7,
+        SequenceEdit    = Key::Step0,
+        Sequence        = Key::Step1,
+        Track           = Key::Step2,
+        Song            = Key::Step3,
+
+        System          = Key::Track7,
+        Monitor         = Key::Step7,
+
+        // Project         = Key::Track0,
+        // Layout          = Key::Track1,
+        // Track           = Key::Track2,
+        // Sequence        = Key::Track3,
+        // SequenceEdit    = Key::Track4,
+        // Song            = Key::Track6,
+        // Routing         = Key::Track7,
+
+        // UserScale       = Key::Step0,
+        // Monitor         = Key::Step6,
+        // System          = Key::Step7,
     };
 
     static bool isPageKey(int code) {
