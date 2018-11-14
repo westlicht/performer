c5645f6f8d290d037816e7303c916164a90b9a37
Simon Kallweit <simon.kallweit@gmail.com>
2018-11-14 18:48:04 +0100

    generator page is modal for now

--- src/apps/sequencer/ui/pages/GeneratorPage.h
+++ src/apps/sequencer/ui/pages/GeneratorPage.h
@@ -19,6 +19,8 @@ public:
     virtual void draw(Canvas &canvas) override;
     virtual void updateLeds(Leds &leds) override;
 
+    virtual bool isModal() const override { return true; }
+
     virtual void keyDown(KeyEvent &event) override;
     virtual void keyUp(KeyEvent &event) override;
     virtual void keyPress(KeyPressEvent &event) override;
