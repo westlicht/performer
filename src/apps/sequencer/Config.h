28fc019c86bdc6009587e52f3192fdeb1558d6ea
Simon Kallweit <simon.kallweit@gmail.com>
2019-07-18 23:38:25 +0200

    updated to version 0.1.25

--- src/apps/sequencer/Config.h
+++ src/apps/sequencer/Config.h
@@ -7,7 +7,7 @@
 #define CONFIG_VERSION_NAME             "PER|FORMER SEQUENCER"
 #define CONFIG_VERSION_MAJOR            0
 #define CONFIG_VERSION_MINOR            1
-#define CONFIG_VERSION_REVISION         24
+#define CONFIG_VERSION_REVISION         25
 
 // Task priorities
 #define CONFIG_DRIVER_TASK_PRIORITY     5
