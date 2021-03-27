fceefd70747017d67e63e521d94a01c189ec50fe
jackpf <jack_farrelly@yahoo.co.uk>
2021-03-27 09:01:47 +0100

    Generic settings

--- src/apps/sequencer/model/UserSettings.cpp
+++ src/apps/sequencer/model/UserSettings.cpp
@@ -1,30 +1,55 @@
 #include <iostream> // TODO remove
 #include "UserSettings.h"
 
-void UserSettings::setBrightness(int brightness) {
-    if (brightness >= MinBrightness && brightness <= MaxBrightness) {
-        std::cout<<"setting brightness "<<brightness<<std::endl;
-        _brightness = brightness;
+void UserSettings::set(int key, int value) {
+    std::cout << "Setting " << key << ": " << value << std::endl;
+    _settings[key].setValue(value);
+}
+
+//void UserSettings::set(int key, int value) {
+//    int i = 0;
+//    for (auto it = _settings.begin(); it != _settings.end(); it++)
+//        _setting.second.reset();
+//    }
+//}
+
+void UserSettings::shift(int key, int shift) {
+    _settings[key].shiftValue(shift);
+}
+
+Setting *UserSettings::get(const char *key) {
+//    return _settings[key].getValue();
+    for (auto &setting : _settings) {
+        if (setting.getKey() == key) {
+            return &setting;
+        }
     }
+    return nullptr;
 }
 
-void UserSettings::shiftBrightness(int shift) {
-    setBrightness(getBrightness() + shift);
+Setting *UserSettings::get(int key) {
+    return &_settings[key];
 }
 
-int &UserSettings::getBrightness() {
-    return _brightness;
+std::vector<Setting> UserSettings::all() {
+    return _settings;
 }
 
 void UserSettings::clear() {
     std::cout<<"clearing brightness"<<std::endl;
-    setBrightness(DefaultBrightness);
+    for (auto &_setting : _settings) {
+        _setting.reset();
+    }
 }
 
 void UserSettings::write(VersionedSerializedWriter &writer) const {
-    writer.write(_brightness);
+    for (auto &setting : _settings) {
+        writer.write(setting.getValue());
+    }
 }
 
 void UserSettings::read(VersionedSerializedReader &reader) {
-    reader.read(_brightness);
+    for (auto &setting : _settings) {
+        reader.read(setting.getValue());
+    }
 }
