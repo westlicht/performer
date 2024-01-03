d37345590ff49eac85d307a5cff3da9cfad54efa
mebitek <mebitek@zohomail.eu>
2024-01-03 07:22:19 +0100

    change pattern page label depending on pattern change option

--- src/apps/sequencer/ui/pages/PatternPage.cpp
+++ src/apps/sequencer/ui/pages/PatternPage.cpp
@@ -50,6 +50,10 @@ void PatternPage::draw(Canvas &canvas) {
     const auto &playState = _project.playState();
     bool hasCancel = playState.hasSyncedRequests() || playState.hasLatchedRequests();
     bool snapshotActive = playState.snapshotActive();
+
+
+    int patternChange = _model.settings().userSettings().get<LaunchpadPatternChange>(SettingLaunchpadPatternChange)->getValue();
+
     const char *functionNames[] = {
         "LATCH",
         "SYNC",
@@ -57,6 +61,9 @@ void PatternPage::draw(Canvas &canvas) {
         snapshotActive ? "COMMIT" : nullptr,
         hasCancel ? "CANCEL" : nullptr
     };
+    if (patternChange==1) {
+        functionNames[1] = "IMMEDIATE";   
+    }
 
     WindowPainter::clear(canvas);
     WindowPainter::drawHeader(canvas, _model, _engine, "PATTERN");
